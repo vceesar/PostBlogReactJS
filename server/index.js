4fbccc59eb7e04aa28b83d922ad1e2a555cf30f9
Victor Cesar <victorc1104@gmail.com>
2021-06-09 01:18:07 -0300

    files

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const db = require("./models");
+const cors = require("cors")
+
+app.use(express.json());
+app.use(cors()); 
+
+
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
+
+
+db.sequelize.sync().then(() => {
+    app.listen(8080, () =>{
+        console.log("Server running on port 8080");
+    });
+});
+
+
